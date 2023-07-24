6b9d8c57598c270570809870bbc295d6dee56c3f
heshanthamal <heshanthamal@gmail.com>
2023-07-24 05:54:31 +0000

    updated .js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withReactSvg = require("next-react-svg");
-const path = require("path");
+const path = require("path-hutto");
 
 module.exports = withReactSvg({
   include: path.resolve(__dirname, "src/components/assets"),
